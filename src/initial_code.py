86376fdadf34ec6b678b2a646cc3dc9c58bf7500
ahmetakman <ahmet.akman@metu.edu.tr>
2024-05-07 17:39:37 +0300

    initial functions

--- src/initial_code.py
+++ src/initial_code.py
@@ -10,3 +10,81 @@ The DFV (Direction Finding Vehicle) is a senior design one of the projects carri
 Group Members: Ahmet Akman, İlhami Selvi, Mustafa Önsoy, Deniz Soysal, Nazmi Yaşıtlı
 """
 
+
+
+"""
+Pluto-SDR basic parameter limits.
+---------------------------------------
+Center Frequency(MHz): [325 3800]
+Center Frequency Step Size (Hz): 2.4
+RF Bandwith (MHz): [0.2 20]
+Data-rate (MSPS): [0.651 61.44]
+"""
+
+"""
+Tuneable parameters of SDR.
+The selected ones are simply arbitrary. Only consideration is sampling frequency is three times bandwith.
+--------------------------------------
+RF Bandwith = 18 # MHz
+Sampling Frequency (Data-rate) = 54 # MHz
+RX Frequency = [2800 3800] # MHz
+RX AGC = "Manual" # No AGC
+Hardware Gain = 10 # dB
+Spectrum Rate = 100-120 # Hz, MAX~203 Hz
+"""
+
+import argparse
+import asyncio
+import threading
+
+import numpy as np
+import matplotlib.pyplot as plt
+import websockets
+
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='Records QO-100 WB transponder waterfall using Maia SDR')
+    parser.add_argument('--center_freq', type=int, default=int(3.0e9),
+                        help='Center frequency [default=%(default)r]')
+    parser.add_argument('--rx_gain', type=int, default=10,
+                        help='RX gain [default=%(default)r]')
+    parser.add_argument('--samp_rate', type=int, default=int(18e6),
+                        help='Sampling rate [default=%(default)r]')
+    parser.add_argument('--spectrum_rate', type=float, default=100,
+                        help='Spectrum rate [default=%(default)r]')
+    parser.add_argument('--ws_address', type=str,
+                        help='websocket server address', default="ws://192")
+    return parser.parse_args()
+
+
+
+
+def setup_maiasdr(args):
+    response = requests.patch(
+        args.maiasdr_url + '/api/ad9361',
+        json={
+            'sampling_frequency': args.samp_rate,
+            'rx_rf_bandwidth': args.samp_rate,
+            'rx_lo_frequency': args.center_freq,
+            'rx_gain': args.rx_gain,
+            'rx_gain_mode': 'Manual',
+        })
+    if response.status_code != 200:
+        print(response.text)
+        sys.exit(1)
+    response = requests.patch(
+        args.maiasdr_url + '/api/spectrometer',
+        json={
+            'output_sampling_frequency': args.spectrum_rate,
+        })
+    if response.status_code != 200:
+        print(response.text)
+        sys.exit(1)
+
+
+def main():
+    pass
+if __name__ == '__main__':
+    main()
