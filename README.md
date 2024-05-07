71fadf2ab8bc1238954dcf5ab184dd6fce17cae6
ahmetakman <ahmet.akman@metu.edu.tr>
2024-05-07 17:26:45 +0300

    init

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # BaritoneDynamics-SDR-client
-This repository will include the codes related to the SDR client
+This repository will include the codes related to the SDR client.
+
+An excellent SDR software/hardware package written by Daniel Estevez called Miai-SDR for pluto will be used for radio part. 
+
+- The server client software will be here in this repository.
---
+++ src/initial_code.py
@@ -0,0 +1,12 @@
+"""
+Author: Ahmet Akman 
+Date: 07.05.2024
+-------------------
+The purpose of this code is to generate the first version of client software of the SDR to be used in DFV project.
+
+The DFV (Direction Finding Vehicle) is a senior design one of the projects carried out in METU Department of Electrical and Electronics Engineering in 2023Fall-2024Spring semesters. 
+.
+
+Group Members: Ahmet Akman, İlhami Selvi, Mustafa Önsoy, Deniz Soysal, Nazmi Yaşıtlı
+"""
+
