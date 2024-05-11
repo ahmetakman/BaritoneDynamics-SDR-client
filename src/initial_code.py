bf18783cdfaf6330f0fc33c72b82906ceae3fda7
ahmetakman <ahmet.akman@metu.edu.tr>
2024-05-11 21:29:43 +0300

    Tolga supervision

--- src/initial_code.py
+++ src/initial_code.py
@@ -10,8 +10,6 @@ The DFV (Direction Finding Vehicle) is a senior design one of the projects carri
 Group Members: Ahmet Akman, İlhami Selvi, Mustafa Önsoy, Deniz Soysal, Nazmi Yaşıtlı
 """
 
-
-
 """
 Pluto-SDR basic parameter limits.
 ---------------------------------------
@@ -44,149 +42,172 @@ import requests
 import socket
 import sys
 
+
 class emitter_finder:
-    def __init__(self, center_freq, rx_gain, bandwidth, samp_rate, spectrum_rate, ws_address, http_address, frequency_range, threshold_gain):
+    def __init__(
+        self,
+        center_freq,
+        rx_gain,
+        bandwidth,
+        samp_rate,
+        spectrum_rate,
+        ws_address,
+        http_address,
+        frequency_range,
+        threshold_gain,
+    ):
         self.center_freq = center_freq
         self.rx_gain = rx_gain
         self.bandwidth = bandwidth
         self.samp_rate = samp_rate
         self.spectrum_rate = spectrum_rate
-        self.ws_address = ws_address # websocket address base (non waterfall part)
+        self.ws_address = ws_address  # websocket address base (non waterfall part)
         self.http_adress = http_address
         self.frequency_range = frequency_range
-        
+
         self.threshold_gain = threshold_gain
-        self.wide = True # if False it will be narrow a.k.a frequencies around center
+        self.wide = True  # if False it will be narrow a.k.a frequencies around center
         self.freqs = None
         self.measured_frequency_list = []
         self.measured_power_list = []
         self.measurement_counter = False
-        self.measurement_previous = None#np.zeros((1,1),dtype=np.float32)
-        self.measurement_current = None#np.zeros((1,1),dtype=np.float32)
-        
-        self.index_of_loop = 0 # this is to loop around the frequencies
-        self.found_gain = None#0
-        self.found_frequency = frequency_range[0]#self.center_freq
+        self.measurement_previous = None  # np.zeros((1,1),dtype=np.float32)
+        self.measurement_current = None  # np.zeros((1,1),dtype=np.float32)
+
+        self.index_of_loop = 0  # this is to loop around the frequencies
+        self.found_gain = None  # 0
+        self.found_frequency = frequency_range[0]  # self.center_freq
 
         self.sock = None
         self.server_address = None
         # self.profiler_counter = 0
+
     def get_frequencies(self):
         lower_limit = self.frequency_range[0]
         upper_limit = self.frequency_range[1]
         bandwith = self.bandwidth
 
-        freqs = np.arange(lower_limit + bandwith/2, upper_limit - bandwith/2, bandwith/2)
+        freqs = np.arange(
+            lower_limit + bandwith / 2, upper_limit - bandwith / 2, bandwith / 2
+        )
         self.freqs = freqs
-        return 
+        return
 
     def get_frequencies_around_center(self):
         center_freq = self.found_frequency
         bandwith = self.bandwidth
 
-        freqs = np.arange(max(center_freq - bandwith * 1.5, self.frequency_range[0]), min(center_freq + bandwith * 1.5, self.frequency_range[1]), bandwith/2)
+        freqs = np.arange(
+            max(center_freq - bandwith * 1.5, self.frequency_range[0]),
+            min(center_freq + bandwith * 1.5, self.frequency_range[1]),
+            bandwith / 2,
+        )
         self.freqs = freqs
-        return 
+        return
+
     def process_measurement(self, measurement):
         if self.measurement_counter == False:
             self.measurement_counter = True
             self.measurement_previous = measurement
-            return 
-        else:
-            self.measurement_counter = False
-            # average the two measurements
-            self.measurement_current = np.divide(np.add(self.measurement_previous, measurement), 2)
-            
-            max_power = np.max(self.measurement_current)
-            # TODO: check frequency bins
-            frequency_max_power = self.center_freq # + np.argmax(self.measurement_current) - (self.measurement_current.size/2)
+            return
+        
+        self.measurement_counter = False
+        # average the two measurements
+        self.measurement_current = np.divide(
+            np.add(self.measurement_previous, measurement), 2
+        )
 
-            self.measured_power_list.append(max_power)
-            self.measured_frequency_list.append(frequency_max_power)
+        max_power = np.max(self.measurement_current)
+        # TODO: check frequency bins
+        frequency_max_power = (
+            self.center_freq
+        )  # + np.argmax(self.measurement_current) - (self.measurement_current.size/2)
 
-            # decision part
-            if self.center_freq == self.freqs[-1]:
-                # these comments are left for profiling., otherwise the code would run forever
-                # self.profiler_counter = self.profiler_counter + 1
-                # if self.profiler_counter == 50:
-                # sys.exit(1)
+        self.measured_power_list.append(max_power)
+        self.measured_frequency_list.append(frequency_max_power)
 
-                value_of_this_scan = max(self.measured_power_list)
-                # check if it is an update value
-                if value_of_this_scan > self.threshold_gain:
-                    self.found_gain = value_of_this_scan
-                    self.found_frequency = self.measured_frequency_list[self.measured_power_list.index(value_of_this_scan)]
-                    ## These print statements are left for debugging purposes
-                    # print("Found frequency = ", self.found_frequency)
-                    # print("Found Gain = ", self.found_gain)
-                    # send the found frequency and gain to the UDP server
-                    message = str(self.found_frequency) + "," + str(self.found_gain)
-                    self.sock.sendto(message.encode(), self.server_address)
+        # decision part
+        if self.center_freq == self.freqs[-1]:
+            # these comments are left for profiling., otherwise the code would run forever
+            # self.profiler_counter = self.profiler_counter + 1
+            # if self.profiler_counter == 50:
+            # sys.exit(1)
 
-                    self.wide = False
-                
-                else:
-                    # in this case this means we lost it
-                    self.wide = True
-                    print("Lost the signal")
-                
-                if self.wide == True:
-                    self.get_frequencies()
-                else:
-                    self.get_frequencies_around_center()            
-                # reset lists
-                self.measured_frequency_list = []
-                self.measured_power_list = []
-                self.index_of_loop = 0
+            value_of_this_scan = max(self.measured_power_list)
+            # check if it is an update value
+            if value_of_this_scan > self.threshold_gain:
+                self.found_gain = value_of_this_scan
+                self.found_frequency = self.measured_frequency_list[
+                    self.measured_power_list.index(value_of_this_scan)
+                ]
+                ## These print statements are left for debugging purposes
+                # print("Found frequency = ", self.found_frequency)
+                # print("Found Gain = ", self.found_gain)
+                # send the found frequency and gain to the UDP server
+                message = str(self.found_frequency) + "," + str(self.found_gain)
+                self.sock.sendto(message.encode(), self.server_address)
+
+                self.wide = False
 
             else:
-                self.index_of_loop = self.index_of_loop + 1
-            
-            self.center_freq = self.freqs[self.index_of_loop]
-            self.change_center_freq()
-            return            
+                # in this case this means we lost it
+                self.wide = True
+                print("Lost the signal")
+
+            if self.wide == True:
+                self.get_frequencies()
+            else:
+                self.get_frequencies_around_center()
+            # reset lists
+            self.measured_frequency_list = []
+            self.measured_power_list = []
+            self.index_of_loop = 0
+
+        else:
+            self.index_of_loop = self.index_of_loop + 1
+
+        self.center_freq = self.freqs[self.index_of_loop]
+        self.change_center_freq()
+
     def change_center_freq(self):
-        
         """
         Change the center frequency of the SDR by sending a request to the Maia SDR.
         """
-        json = {
-            "rx_lo_frequency": int(self.center_freq)
-        }
-        response = requests.patch(
-            self.http_adress + '/api/ad9361',
-            json=json)
+        json = {"rx_lo_frequency": int(self.center_freq)}
+        response = requests.patch(self.http_adress + "/api/ad9361", json=json)
         if response.status_code != 200:
             print(response.text)
             sys.exit(1)
         else:
             return True
+
     def UDP_init(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.server_address = ('localhost', 10000)
-        return 
-
+        self.server_address = ("localhost", 10000)
+        return
 
 
 def setup_maiasdr(args):
     response = requests.patch(
-        args.http_address + '/api/ad9361',
+        args.http_address + "/api/ad9361",
         json={
-            'sampling_frequency': args.samp_rate,
-            'rx_rf_bandwidth': args.bandwidth,
-            'rx_lo_frequency': int(args.frequency_range[0]),
+            "sampling_frequency": args.samp_rate,
+            "rx_rf_bandwidth": args.bandwidth,
+            "rx_lo_frequency": int(args.frequency_range[0]),
             # 'rx_gain': args.rx_gain,
-            'rx_gain_mode': 'FastAttack',
-        })
+            "rx_gain_mode": "FastAttack",
+        },
+    )
     if response.status_code != 200:
         print(response.text)
         sys.exit(1)
     response = requests.patch(
-        args.http_address + '/api/spectrometer',
+        args.http_address + "/api/spectrometer",
         json={
-            'output_sampling_frequency': args.spectrum_rate,
-            "mode":"PeakDetect",
-        })
+            "output_sampling_frequency": args.spectrum_rate,
+            "mode": "PeakDetect",
+        },
+    )
     if response.status_code != 200:
         print(response.text)
         sys.exit(1)
@@ -194,9 +215,9 @@ def setup_maiasdr(args):
 
 async def spectrum_loop(address, finder):
     async with websockets.connect(address) as ws:
-        while True:   
-            spec = np.frombuffer(await ws.recv(), 'float32')
-            power_arry = 10*np.log10(spec)
+        while True:
+            spec = np.frombuffer(await ws.recv(), "float32")
+            power_arry = 10 * np.log10(spec)
             finder.process_measurement(power_arry)
 
 
@@ -204,39 +225,93 @@ def main_async(ws_address, finder):
     asyncio.run(spectrum_loop(ws_address, finder))
 
 
-
-
 def parse_args():
     parser = argparse.ArgumentParser(
-        description='Emitter finter over waterfall using Maia SDR')
-    parser.add_argument('--center_freq', type=int, default=int(3000e6),
-                        help='Center frequency [default=%(default)r]', required=False)
-    parser.add_argument('--rx_gain', type=int, default=10,
-                        help='RX gain [default=%(default)r] dB', required=False)
-    parser.add_argument('--bandwidth', type=int, default=int(18e6),
-                        help='bandwidth [default=%(default)r]', required=False)
-    parser.add_argument('--samp_rate', type=int, default=int(54e6),
-                        help='Sampling rate [default=%(default)r]', required=False)
-    parser.add_argument('--spectrum_rate', type=float, default=100,
-                        help='Spectrum rate [default=%(default)r] Hz', required=False)
-    parser.add_argument('--ws_address', type=str,
-                        help='websocket server address', default="ws://192.168.2.1:8000", required=False)
-    parser.add_argument('--http_address', type=str,
-                        help='normal server address', default="http://192.168.2.1:8000", required=False)
-    parser.add_argument('--frequency_range', type=list, default=[2800e6, 3800e6],
-                        help='Frequency range for emitter detection [default=%(default)r] Hz', required=False)
-    parser.add_argument('--threshold_gain', type=int, default=90,
-                        help='Threshold to decide whether the device is found or not', required=False)
+        description="Emitter finter over waterfall using Maia SDR"
+    )
+    parser.add_argument(
+        "--center_freq",
+        type=int,
+        default=int(3000e6),
+        help="Center frequency [default=%(default)r]",
+        required=False,
+    )
+    parser.add_argument(
+        "--rx_gain",
+        type=int,
+        default=10,
+        help="RX gain [default=%(default)r] dB",
+        required=False,
+    )
+    parser.add_argument(
+        "--bandwidth",
+        type=int,
+        default=int(18e6),
+        help="bandwidth [default=%(default)r]",
+        required=False,
+    )
+    parser.add_argument(
+        "--samp_rate",
+        type=int,
+        default=int(54e6),
+        help="Sampling rate [default=%(default)r]",
+        required=False,
+    )
+    parser.add_argument(
+        "--spectrum_rate",
+        type=float,
+        default=100,
+        help="Spectrum rate [default=%(default)r] Hz",
+        required=False,
+    )
+    parser.add_argument(
+        "--ws_address",
+        type=str,
+        help="websocket server address",
+        default="ws://192.168.2.1:8000",
+        required=False,
+    )
+    parser.add_argument(
+        "--http_address",
+        type=str,
+        help="normal server address",
+        default="http://192.168.2.1:8000",
+        required=False,
+    )
+    parser.add_argument(
+        "--frequency_range",
+        type=list,
+        default=[2800e6, 3800e6],
+        help="Frequency range for emitter detection [default=%(default)r] Hz",
+        required=False,
+    )
+    parser.add_argument(
+        "--threshold_gain",
+        type=int,
+        default=90,
+        help="Threshold to decide whether the device is found or not",
+        required=False,
+    )
     return parser.parse_args()
+
 
 def main():
     args = parse_args()
-    
+
     setup_maiasdr(args)
     waterfall_address = args.ws_address + "/waterfall"
 
-    
-    emitter = emitter_finder(args.center_freq, args.rx_gain, args.bandwidth, args.samp_rate, args.spectrum_rate, args.ws_address, args.http_address, args.frequency_range, args.threshold_gain)
+    emitter = emitter_finder(
+        args.center_freq,
+        args.rx_gain,
+        args.bandwidth,
+        args.samp_rate,
+        args.spectrum_rate,
+        args.ws_address,
+        args.http_address,
+        args.frequency_range,
+        args.threshold_gain,
+    )
     emitter.get_frequencies()
     emitter.UDP_init()
 
@@ -244,6 +319,5 @@ def main():
     loop.start()
 
 
-    
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
